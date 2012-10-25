729fc3513002b694c05f9cb74675de95456fb01f
Kasper Isager <kasperisager@gmail.com>
2012-10-25 14:42:40 +0200

    Fix a typo in all custom.* files

--- src/javascript/custom.js
+++ src/javascript/custom.js
@@ -1,5 +1,5 @@
 //  This file actually doesn't do anything...
 //  Feel free to fill it with your own scripts!
 
-//  Add you customizations beyond this point
-//  ----------------------------------------
+//  Add your customizations beyond this point
+//  -----------------------------------------
