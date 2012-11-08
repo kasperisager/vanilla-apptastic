5572174748965fe4e7a94060e836dd457b65dc4b
Kasper Isager <kasperisager@gmail.com>
2012-11-08 12:46:03 +0100

    Update description

--- about.php
+++ about.php
@@ -3,7 +3,7 @@
 $ThemeInfo["Apptastic"] = array(
 
 	"Name" => "Apptastic",
-	"Description" => "A sleek and super modern desktop-like interface for Vanilla. Built using Austere and Compass",
+	"Description" => "A sleek and super modern desktop-like interface for Vanilla derived from the <b><a href='http://getsentry.com/welcome/'>Sentry</a></b> application. The theme is based on the <b><a href='http://twitter.github.com/bootstrap/'>Bootstrap framework</a></b> by <b><a href='http://twitter.com'>Twitter</a></b> and built using <b><a href='https://github.com/kasperisager/Austere'>Austere</a></b> as well as the <b><a href='http://compass-style.org'>Compass</a></b> framework.",
 	"Version" => "0.9.1",
 	"Author" => "Kasper K. Isager",
 	"AuthorEmail" => "kasperisager@gmail.com",
