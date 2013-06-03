5ee4bfa25ef0d8e4a477267835704bec7dddcac7
Chris Ireland <chris@tama63.co.uk>
2013-06-03 22:29:47 +0100

    Fixing OpenID
    makes text easier to see when using an OpenID Auth

--- about.php
+++ about.php
@@ -4,7 +4,7 @@ $ThemeInfo["Apptastic"] = array(
 
 	"Name" => "Apptastic",
 	"Description" => "A sleek and super modern desktop-like interface for Vanilla derived from the <b><a href='http://getsentry.com/welcome/'>Sentry</a></b> application. The theme is based on the <b><a href='http://twitter.github.com/bootstrap/'>Bootstrap framework</a></b> by <b><a href='http://twitter.com'>Twitter</a></b> and built using <b><a href='https://github.com/kasperisager/Austere'>Austere</a></b> as well as the <b><a href='http://compass-style.org'>Compass</a></b> framework.<p></p> This is NOT a ready-to-use theme and is merely meant as a canvas of ideas. Feel free to fork the theme on Github and send me a pull request if you have some great code you want to share.",
-	"Version" => "0.9.2",
+	"Version" => "0.9.2.1",
 	"Author" => "Kasper K. Isager",
 	"AuthorEmail" => "kasperisager@gmail.com",
 	"AuthorUrl" => "https://github.com/kasperisager",
