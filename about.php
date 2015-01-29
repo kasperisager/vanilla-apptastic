7f75b28fce12c7525387cf361c8c563d6562847f
Kasper Isager <kasperisager@gmail.com>
2015-01-29 21:10:33 +0100

    Update about.php

--- about.php
+++ about.php
@@ -2,7 +2,7 @@
 
 $ThemeInfo['apptastic'] = array(
   'Name'        => "Apptastic",
-  'Description' => "A sleek and modern theme for Vanilla built on Bootstrap. Features an experimental, 3-column layout.",
+  'Description' => "A sleek and modern theme for Vanilla built on Bootstrap. Based on the design of ApplyBin. Features an experimental, 3-column layout.",
   'Version'     => '1.0.0',
   'Author'      => "Kasper Kronborg Isager",
   'AuthorEmail' => 'kasperisager@gmail.com',
